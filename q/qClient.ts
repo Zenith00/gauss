e5be58260a07f57644789f178a7ca3b606fa3dfa
Zenith <z@zenith.dev>
2024-03-17 00:42:50 -0700

    s

--- q/qClient.ts
+++ q/qClient.ts
@@ -14,7 +14,7 @@ myWorker.onmessage = (e) => {
     let atLeast = new Fraction(1);
     let atMost = new Fraction(0);
     [...damageResult.regularDamagePMF.entries()].forEach(([damage, prob]) => {
-        table += `<tr><td>${damage}</td><td>${prob.toString()}</td><td>${atLeast.toString()}</td><td>${atMost}</td></tr>`;
+        table += `<tr><td>${damage}</td><td>${prob}</td><td>${atLeast}</td><td>${atMost}</td></tr>`;
         atLeast = atLeast.sub(prob);
         atMost = atMost.add(prob);
     });
